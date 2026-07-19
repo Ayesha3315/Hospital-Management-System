38003403bfd0d1de79afb21a4de5744d68caa279
Ayesha Fatima <ayesha.fatima3315l@gmail.com>
2026-07-19 13:42:52 +0530

    Created the Doctor,Bill and Appointment entity

---
+++ Hospital-Management-System/src/main/java/com/ayesha/Hospital/Management/System/models/Appointment.java
@@ -0,0 +1,23 @@
+package com.ayesha.Hospital.Management.System.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private long patientId;
+    private long doctorId;
+    private String date;
+
+}
